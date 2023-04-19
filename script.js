1bec3ccdb3f11b9fd9df06ba15efb06596e2c061
Joey Gennari <joey.gennari@lennoxintl.com>
2023-04-19 10:59:23 -0400

    Fixed year bug

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('generation-form').addEventListener('submit', (event) =>
     event.preventDefault();
     const input = document.getElementById('input').value;
     const currentYear = new Date().getFullYear();
-    const yearOfBirth = isNaN(input) ? currentYear - parseInt(input, 10) : parseInt(input, 10);
+    const yearOfBirth = parseInt(input, 10) < 120 ? currentYear - parseInt(input, 10) : parseInt(input, 10);
     const generation = getGeneration(yearOfBirth);
     const popularShows = getPopularShows(yearOfBirth);
     document.getElementById('output').innerHTML = `Generation: ${generation}<br>Popular Shows: ${popularShows.join(', ')}`;
